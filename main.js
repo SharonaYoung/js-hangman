0091ec05e08dae3188e0a90741b9afb3faa141cc
Sharona Young <msrona806@gmail.com>
2023-01-15 23:48:06 -0500

    wrapped code in a function to play game

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
 const guessesEl = document.querySelector('#guesses');
 guessesEl.textContent = 6;
-let guessesRemaining = 6;
+
 
 // array of possible words
-const wordbank = ["minneapolis", "houston", "honolulu", "orlando", "chicago", "denver", "vancouver", "baltimore", "richmond", "fresno"];
+const wordbank = ["minneapolis", "houston", "honolulu", "orlando", "chicago", "denver", "vancouver", "baltimore", "richmond", "fresno", "boston", "atlanta"];
 
-// choose random word from array
-const randomWord = wordbank[Math.floor(Math.random() * 10)];
-console.log(randomWord);
 
-// display a line for letters of word
-let wordToGuess = '';
-for (let i = 0; i < randomWord.length; i++){
-  wordToGuess += "_";
-}
 
-// target element where word on the page will be displayed
-const wordEl = document.querySelector("#word");
-wordEl.textContent = wordToGuess;
+const play = () => {
+  let guessesRemaining = 6;
+  // choose random word from array
+  const randomWord = wordbank[Math.floor(Math.random() * wordbank.length)];
+  console.log(randomWord);
 
-// function to capture keyboard event
-document.body.addEventListener('keydown', function(event){
-  const letterIndex = randomWord.indexOf(event.key); 
-  // compare user guess to word to guess:
-  // decrease remaining guesses if letter incorrect
-  if(letterIndex === -1){
-    guessesRemaining --;
-    guessesEl.textContent = guessesRemaining;
-    
+  // display a line for letters of word
+  let wordToGuess = '';
+  for (let i = 0; i < randomWord.length; i++){
+    wordToGuess += "_";
   }
-});
-// let wordArray = wordToGuess.split('');
+
+  // target element where word on the page will be displayed
+  const wordEl = document.querySelector("#word");
+  wordEl.textContent = wordToGuess;
+
+  // function to capture keyboard event
+  document.body.addEventListener('keydown', function(event){
+    // get the index of the key pressed
+    const letterIndex = randomWord.indexOf(event.key); 
+
+    // compare user guess to word to guess:
+    // decrease remaining guesses if letter incorrect
+    if(letterIndex === -1){
+      guessesRemaining --;
+      guessesEl.textContent = guessesRemaining;  
+    }else{
+        // if correct letter guessed, determine place in word
+        let word = wordToGuess.split('');
+        for(let i = 0; i < wordToGuess.length; i++){
+          if(event.key === randomWord[i]){
+            word[i] = randomWord[i];
+        }
+      }      
+        wordToGuess = word.join(' ');
+        wordEl.textContent = wordToGuess;
+    } 
+  });   
+}
 
 // function to track score 
 
+play();
